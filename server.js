0c6f1f8f91e9179ddb2a9af4b107e0e4b3b10bc2
Abu <55980260+Abu-22@users.noreply.github.com>
2020-09-24 16:21:39 +0200

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express(),
       bodyParser = require("body-parser");
-      port = 443;
+      port = 8080;
       const path = require('path');
 const users = [];
 
